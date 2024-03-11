ddec42d7ac406f781390125317ef9f2554730846
prem-acharya <premacharya2193@gmail.com>
2024-03-11 17:49:27 +0530

    Improving the user interface (UI) of login and sign-up screens

--- src/Components/Authentication/Login.jsx
+++ src/Components/Authentication/Login.jsx
@@ -1,32 +1,27 @@
-import React, { useState, useEffect } from "react";
-import { auth } from "../../firebase";
+import React, { useState } from "react";
+import { auth, firestore, googleProvider } from "../../firebase";
 import { useNavigate, Link } from "react-router-dom";
 import {
   signInWithEmailAndPassword,
   sendPasswordResetEmail,
+  signInWithPopup,
+  GoogleAuthProvider, // Added GoogleAuthProvider
 } from "firebase/auth";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { doc, setDoc } from "firebase/firestore";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import VisibilityIcon from "@mui/icons-material/Visibility";
+import loginGif from "../../assets/Login.gif";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [isLogin, setIsLogin] = useState(false);
-  // const [theme, setTheme] = useState("light");
   const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   const prefersDarkMode =
-  //     window.matchMedia &&
-  //     window.matchMedia("(prefers-color-scheme: dark)").matches;
-  //   setTheme(prefersDarkMode ? "dark" : "light");
-  // }, []);
-
   const handleLogin = async () => {
-
     setIsLogin(true);
 
     try {
@@ -36,7 +31,6 @@ const Login = () => {
         navigate("/dashboard");
       }, 2000);
     } catch (error) {
-      // console.error('Login failed', error.message);
       toast.error("Invalid: ID & Password");
     } finally {
       setIsLogin(false);
@@ -48,67 +42,102 @@ const Login = () => {
       await sendPasswordResetEmail(auth, email);
       toast.success("Email sent For Password Reset 📧");
     } catch (error) {
-      // console.error('Password reset failed', error.message);
       toast.error("Failed: Check Your Email For Password Reset");
     }
   };
 
+  const handleGoogleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+      const { user } = result;
+  
+      if (user.metadata.creationTime === user.metadata.lastSignInTime) {
+        await setDoc(doc(firestore, "users", user.uid), {
+          username: user.displayName,
+          email: user.email,
+        });
+      }
+  
+      toast.success("Google Login Successful 🥳");
+      setTimeout(() => {
+        navigate("/dashboard");
+      }, 3000);
+    } catch (error) {
+      toast.error("Google Login Failed");
+    }
+  };
+
   return (
-    <div
-      className="max-w-md mx-auto mt-24 p-6 rounded-md bg-white shadow-custom">
-      <h2 className="text-2xl font-semibold mb-4">Login</h2>
-      <input
-        className="w-full text-slate-800 mb-3 p-2 border rounded-md"
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <div className="relative">
-        <input
-          className="w-full mb-3 p-2 text-slate-800 border rounded-md"
-          type={showPassword ? "text" : "password"}
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <span
-          onClick={() => setShowPassword(!showPassword)}
-          className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-4"
-        >
-          {showPassword ? (
-            <VisibilityIcon
-              className="text-gray-700"
-              style={{ marginBottom: "12px" }}
+    <div className="min-h-screen flex items-center justify-center space-x-4 bg-gray-100">
+      <div className="flex flex-col md:flex-row bg-white rounded-xl shadow-custom overflow-hidden">
+        <div className="hidden md:block w-full md:w-1/2 lg:w-3/5">
+          <img className="object-cover w-full h-full" src={loginGif} alt="Login" />
+        </div>
+        <div className="w-full md:max-w-md lg:max-w-full md:w-1/2 lg:w-3/5 p-8 md:p-12 lg:p-24">
+          <h2 className="text-2xl font-semibold mb-4">Hello Again!</h2>
+          <p className="text-sm mb-8">Welcome back you've been missed!</p>
+          <input
+            className="w-full text-slate-800 mb-3 p-2 border rounded-md"
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <div className="relative">
+            <input
+              className="w-full mb-3 p-2 text-slate-800 border rounded-md"
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
-          ) : (
-            <VisibilityOffIcon
-              className="text-gray-500"
-              style={{ marginBottom: "12px" }}
-            />
-          )}
-        </span>
+            <span
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-4"
+            >
+              {showPassword ? (
+                <VisibilityOffIcon
+                  className="text-gray-700"
+                  style={{ marginBottom: "12px" }}
+                />
+              ) : (
+                <VisibilityIcon
+                  className="text-gray-500"
+                  style={{ marginBottom: "12px" }}
+                />
+              )}
+            </span>
+          </div>
+          <button
+            className="w-full bg-blue-500 text-white p-2 mt-2 font-semibold rounded-md mb-4 hover:bg-blue-600"
+            disabled={isLogin}
+            onClick={handleLogin}
+          >
+            {isLogin ? "Login..." : "Login"}
+          </button>
+          <button
+            className="w-full bg-gray-300 text-gray-800 p-2 font-semibold rounded-md mb-2 hover:bg-gray-400"
+            onClick={handleForgotPassword}
+          >
+            Forgot Password
+          </button>
+          <div className="text-center mb-2 font-light">OR</div>
+          <button
+            type="button"
+            className="w-full bg-gray-500 text-white p-2 font-semibold rounded-md hover:bg-gray-700"
+            onClick={handleGoogleLogin}
+          >
+            Login with Google
+          </button>
+          <div className="text-center mt-3 font-light">
+            Don't have an account?{" "}
+            <Link to="/signup" className="font-semibold hover:underline">
+              Create Account
+            </Link>
+          </div>
+          <ToastContainer />
+        </div>
       </div>
-      <button
-        className="w-full bg-blue-500 text-white p-2 mt-2 font-semibold rounded-md mb-4 hover:bg-blue-600"
-        disabled={isLogin}
-        onClick={handleLogin}
-      >
-        {isLogin ? "Login..." : "Login"}
-      </button>
-      <button
-        className="w-full bg-gray-300 text-gray-800 p-2 font-semibold rounded-md mb-4 hover:bg-gray-400"
-        onClick={handleForgotPassword}
-      >
-        Forgot Password
-      </button>
-      <div className="text-center font-light">
-        Don't have an account?{" "}
-        <Link to="/signup" className="font-semibold hover:underline">
-          Create Account
-        </Link>
-      </div>
-      <ToastContainer />
     </div>
   );
 };
--- src/Components/Authentication/SignUp.jsx
+++ src/Components/Authentication/SignUp.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import { auth, firestore, googleProvider } from "../../firebase";
+import { auth, firestore } from "../../firebase";
 import {
   createUserWithEmailAndPassword,
   sendEmailVerification,
@@ -13,6 +13,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import verify from "../../assets/Verified.gif";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import VisibilityIcon from "@mui/icons-material/Visibility";
+import signup from "../../assets/Signup.gif"
 
 const Signup = () => {
   const [username, setUsername] = useState("");
@@ -26,13 +27,6 @@ const Signup = () => {
   const [passwordStrength, setPasswordStrength] = useState("");
   const [isSigningUp, setIsSigningUp] = useState(false);
   const navigate = useNavigate();
-
-  // useEffect(() => {
-  //   const prefersDarkMode =
-  //     window.matchMedia &&
-  //     window.matchMedia("(prefers-color-scheme: dark)").matches;
-  //   setTheme(prefersDarkMode ? "dark" : "light");
-  // }, []);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -109,7 +103,6 @@ const Signup = () => {
         username: username,
         email: email,
       });
-      // toast.success("Signup successful 🥳", { duration: 8000 });
       toast.success("Verification Email Sent 📧");
       setIsEmailSent(true);
     } catch (error) {
@@ -119,163 +112,133 @@ const Signup = () => {
     }
   };
 
-  const handleGoogleSignup = async () => {
-    try {
-      const result = await signInWithPopup(auth, googleProvider);
-      const { user } = result;
-  
-      // Check if the user is new (just signed up)
-      if (user.metadata.creationTime === user.metadata.lastSignInTime) {
-        // Set additional information in Firestore
-        await setDoc(doc(firestore, "users", user.uid), {
-          username: user.displayName,
-          email: user.email,
-        });
-      }
-  
-      toast.success("Google SignUp Successful 🥳");
-      setTimeout(() => {
-        navigate("/dashboard");
-      }, 3000);
-    } catch (error) {
-      toast.error("Google SignUp Failed");
-    }
-  };
-  
-    
-
   return (
-    <div>
-      {!isEmailSent ? (
-        <form onSubmit={handleSignup}>
-          <div
-            className="max-w-md mx-auto mt-24 p-6 rounded-md shadow-xl bg-white" >
-            <div className="text-2xl font-semibold mb-4">Signup</div>
-            <input
-              className="w-full mb-3 p-2 text-slate-800 border rounded-md"
-              type="text"
-              placeholder="Username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <input
-              className="w-full mb-3 p-2 text-slate-800 border rounded-md"
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <div className="relative">
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="flex flex-col lg:flex-row rounded-xl bg-white shadow-custom overflow-hidden">
+        <div className="hidden lg:block lg:w-1/2">
+          <img className="object-cover w-full h-full" src={signup} alt="SignUp" />
+        </div>
+        <div className="w-full lg:max-w-md p-8 md:p-12 lg:p-24">
+          {!isEmailSent ? (
+            <form className="m-[-20px]" onSubmit={handleSignup}>
+              <h2 className="text-2xl font-semibold mb-4">Sign Up</h2>
+              <p className="text-sm mb-8">Join us! You've been missed!</p>
               <input
                 className="w-full mb-3 p-2 text-slate-800 border rounded-md"
-                type={showPassword ? "text" : "password"}
-                placeholder="Password"
-                value={password}
-                onChange={handlePasswordChange}
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
               />
-              <span
-                onClick={() => setShowPassword(!showPassword)}
-                className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-4"
-              >
-                {showPassword ? (
-                  <VisibilityIcon
-                    className="text-gray-700"
-                    style={{ marginBottom: "12px" }}
-                  />
-                ) : (
-                  <VisibilityOffIcon
-                    className="text-gray-500"
-                    style={{ marginBottom: "12px" }}
-                  />
-                )}
-              </span>
-            </div>
-            <div className="relative">
               <input
-                className="w-full mb-3 p-2 bg-white shadow-sm text-slate-800 border rounded-md"
-                type={showconfirmPassword ? "text" : "password"}
-                placeholder="Confirm Password"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
+                className="w-full mb-3 p-2 text-slate-800 border rounded-md"
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
-              <span
-                onClick={() => setShowconfirmPassword(!showconfirmPassword)}
-                className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-4"
-              >
-                {showconfirmPassword ? (
-                  <VisibilityIcon
-                    className="text-gray-700"
-                    style={{ marginBottom: "12px" }}
-                  />
-                ) : (
-                  <VisibilityOffIcon
-                    className="text-gray-500"
-                    style={{ marginBottom: "12px" }}
-                  />
-                )}
-              </span>
-            </div>
-            <div className="flex font-light">
-              Password strength:
-              <div
-                className={`ml-1 font-semibold ${
-                  passwordStrength === "Weak"
-                    ? "text-red-500"
-                    : passwordStrength === "Medium"
-                    ? "text-yellow-500"
-                    : "text-green-500"
-                }`}
-              >
-                {passwordStrength}
+              <div className="relative">
+                <input
+                  className="w-full mb-3 p-2 text-slate-800 border rounded-md"
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                />
+                <span
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-4"
+                >
+                  {showPassword ? (
+                    <VisibilityOffIcon
+                      className="text-gray-700"
+                      style={{ marginBottom: "12px" }}
+                    />
+                  ) : (
+                    <VisibilityIcon
+                      className="text-gray-500"
+                      style={{ marginBottom: "12px" }}
+                    />
+                  )}
+                </span>
               </div>
+              <div className="relative">
+                <input
+                  className="w-full mb-3 p-2 bg-white shadow-sm text-slate-800 border rounded-md"
+                  type={showconfirmPassword ? "text" : "password"}
+                  placeholder="Confirm Password"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+                <span
+                  onClick={() => setShowconfirmPassword(!showconfirmPassword)}
+                  className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-4"
+                >
+                  {showconfirmPassword ? (
+                    <VisibilityOffIcon
+                      className="text-gray-700"
+                      style={{ marginBottom: "12px" }}
+                    />
+                  ) : (
+                    <VisibilityIcon
+                      className="text-gray-500"
+                      style={{ marginBottom: "12px" }}
+                    />
+                  )}
+                </span>
+              </div>
+              <div className="flex font-light">
+                Password strength:
+                <div
+                  className={`ml-1 font-semibold ${
+                    passwordStrength === "Weak"
+                      ? "text-red-500"
+                      : passwordStrength === "Medium"
+                      ? "text-yellow-500"
+                      : "text-green-500"
+                  }`}
+                >
+                  {passwordStrength}
+                </div>
+              </div>
+              <button
+                type="submit"
+                disabled={isSigningUp}
+                className="w-full bg-blue-500 text-white p-2 mt-4 font-semibold rounded-md mb-2 hover:bg-blue-600"
+              >
+                {isSigningUp ? "Signing up..." : "Signup"}
+              </button>
+              <div className="text-center mt-3 font-light">
+                Already have an account?{" "}
+                <Link to="/login" className="font-semibold hover:underline">
+                  Log In
+                </Link>
+              </div>
+            </form>
+          ) : (
+            <div className="text-center">
+              <h2 className="text-2xl font-semibold mb-4">Verify Your Email</h2>
+              <p className="font-semibold">
+                Please check your email inbox for the verification link.
+              </p>
+              <p className="mt-2">
+                Verification email sent to:{" "}
+                <strong className="text-blue-500">{email}</strong>
+              </p>
+              <div>
+                <img src={verify} alt="Email verification" />
+              </div>
+              <p className="mt-2 font-light">
+                Have an account?{" "}
+                <Link to="/login" className="font-semibold hover:underline">
+                  Log In
+                </Link>
+              </p>
             </div>
-            <button
-              type="submit"
-              disabled={isSigningUp}
-              className="w-full bg-blue-500 text-white p-2 mt-4 font-semibold rounded-md mb-2 hover:bg-blue-600"
-            >
-              {isSigningUp ? "Signing up..." : "Signup"}
-            </button>
-            <div className="text-center mb-2 font-light">
-              OR
-            </div>
-            <button
-              type="button"
-              className="w-full bg-gray-500 text-white p-2 font-semibold rounded-md hover:bg-gray-700"
-              onClick={handleGoogleSignup}
-            >
-              Signup with Google
-            </button>
-            <div className="text-center mt-4 font-light">
-              Back to{" "}
-              <Link to="/login" className="font-semibold hover:underline">
-                Login
-              </Link>
-            </div>
-          </div>
-        </form>
-      ) : (
-        <div className="max-w-md mx-auto mt-24 p-6 text-center bg-white rounded-md shadow-custom">
-          <div className="text-2xl font-semibold mb-4">Verify Your Email</div>
-          <div className="font-semibold">
-            Please check your email inbox for the verification link.
-          </div>
-          <div className="mt-2">
-            Verification email sent to:{" "}
-            <strong className="text-blue-500">{email}</strong>
-          </div>
-          <div>
-            <img src={verify} alt="Email verification" />
-          </div>
-          <div className="mt-2 font-light">
-            Have an account?{" "}
-            <Link to="/login" className="font-semibold hover:underline">
-              Log In
-            </Link>
-          </div>
+          )}
+          <ToastContainer />
         </div>
-      )}
-      <ToastContainer />
+      </div>
     </div>
   );
 };
