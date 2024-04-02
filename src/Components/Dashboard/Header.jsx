5b63a3f96689674c882fb64294f8bb884355da38
Prem Acharya <premacharya2193@gmail.com>
2024-04-02 17:59:40 +0530

    Added search functionality

--- src/Components/Dashboard/Header.jsx
+++ src/Components/Dashboard/Header.jsx
@@ -5,7 +5,7 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import MenuIcon from "@mui/icons-material/Menu";
 import Notes from "../../assets/logo1.png";
 import { firestore as db } from "../../firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, collection, query, where, getDocs } from "firebase/firestore"; // Import additional Firestore functions
 import { useAuth } from "../Authentication/AuthContext";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import { signOut, getAuth } from "firebase/auth";
@@ -13,7 +13,19 @@ import ClearIcon from "@mui/icons-material/Clear";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import LoadingBar from "react-top-loading-bar"; // Import LoadingBar
-// import { Link } from "react-router-dom";
+import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
+import PictureAsPdfIcon from "@mui/icons-material/PictureAsPdf";
+import ImageIcon from "@mui/icons-material/Image";
+import MovieIcon from "@mui/icons-material/Movie";
+import MusicVideoIcon from "@mui/icons-material/MusicVideo";
+import DescriptionIcon from "@mui/icons-material/Description";
+import SlideshowIcon from "@mui/icons-material/Slideshow";
+import TableChartIcon from "@mui/icons-material/TableChart";
+import GifBoxIcon from "@mui/icons-material/GifBox";
+import FolderZipIcon from "@mui/icons-material/FolderZip";
+import TerminalIcon from "@mui/icons-material/Terminal";
+import FolderIcon from "@mui/icons-material/Folder"; // Import FolderIcon for folders
+import { useNavigate } from 'react-router-dom';
 
 const Header = ({ isSidebarOpen, setIsSidebarOpen }) => {
   const [username, setUsername] = useState("");
@@ -21,6 +33,10 @@ const Header = ({ isSidebarOpen, setIsSidebarOpen }) => {
   const [showUserInfo, setShowUserInfo] = useState(false);
   const [progress, setProgress] = useState(0); // State for loading bar progress
   const auth = getAuth();
+  const [searchValue, setSearchValue] = useState(""); // Add this line
+  const [searchResults, setSearchResults] = useState([]); // State to hold both files and folders
+  const [isFocused, setIsFocused] = useState(false); // State to manage input focus
+  const navigate = useNavigate();
 
   const user = auth.currentUser;
 
@@ -67,9 +83,111 @@ const Header = ({ isSidebarOpen, setIsSidebarOpen }) => {
     }
   }, [currentUser]); // Depend on currentUser to re-run the effect when it changes
 
+  useEffect(() => {
+    if (currentUser && searchValue.trim() !== "") {
+      const fetchSearchResults = async () => {
+        try {
+          // Query for files
+          const filesQuery = query(collection(db, "files"), where("userId", "==", currentUser.uid));
+          const filesSnapshot = await getDocs(filesQuery);
+          const files = filesSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data(), type: 'file' }));
+
+          // Query for folders (assuming folders are stored in a "folders" collection)
+          const foldersQuery = query(collection(db, "folders"), where("userId", "==", currentUser.uid));
+          const foldersSnapshot = await getDocs(foldersQuery);
+          const folders = foldersSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data(), type: 'folder' }));
+
+          // Combine files and folders
+          const combinedResults = [...folders, ...files]; // Note that folders are added first
+
+          // Filter based on the current searchValue
+          const searchLower = searchValue.toLowerCase();
+          const filteredResults = combinedResults.filter((item) =>
+            item.name.toLowerCase().startsWith(searchLower)
+          );
+
+          // Sort so that folders appear first
+          const sortedResults = filteredResults.sort((a, b) => {
+            if (a.type === 'folder' && b.type === 'file') {
+              return -1;
+            }
+            if (a.type === 'file' && b.type === 'folder') {
+              return 1;
+            }
+            return a.name.localeCompare(b.name); // Optionally, sort by name within each type
+          });
+
+          setSearchResults(sortedResults);
+        } catch (error) {
+          toast.error("Error fetching search results");
+        }
+      };
+
+      fetchSearchResults();
+    } else {
+      setSearchResults([]);
+    }
+  }, [currentUser, searchValue]);
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setTimeout(() => {
+      setIsFocused(false);
+    }, 100);
+  };
+
   const isDesktopOrLaptop = useMediaQuery({ query: "(min-width: 768px)" });
   const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
   const isMobile = useMediaQuery({ query: "(max-width: 525px)" });
+
+  // Function to get the appropriate file icon based on file extension
+  const getFileIcon = (fileName) => {
+    const extension = fileName.split(".").pop().toLowerCase();
+    switch (extension) {
+      case "pdf":
+        return <PictureAsPdfIcon className="text-blue-400" />;
+      case "png":
+      case "jpg":
+      case "jpeg":
+        return <ImageIcon className="text-blue-400" />;
+      case "mp4":
+        return <MovieIcon className="text-blue-400" />;
+      case "mp3":
+        return <MusicVideoIcon className="text-blue-400" />;
+      case "docx":
+        return <DescriptionIcon className="text-blue-400" />;
+      case "pptx":
+        return <SlideshowIcon className="text-blue-400" />;
+      case "xlsx":
+        return <TableChartIcon className="text-blue-400" />;
+      case "gif":
+        return <GifBoxIcon className="text-blue-400" />;
+      case "zip":
+        return <FolderZipIcon className="text-blue-400" />;
+      case "py":
+      case "c":
+      case "html":
+      case "c#":
+      case "cpp":
+      case "css":
+      case "js":
+      case "jsx":
+      case "ts":
+      case "tsx":
+      case "json":
+      case "dart":
+      case "md":
+      case "yaml":
+      case "cc":
+      case "php":
+        return <TerminalIcon className="text-blue-400" />;
+      default:
+        return <InsertDriveFileIcon className="text-blue-400" />;
+    }
+  };
 
   return (
     <>
@@ -88,15 +206,42 @@ const Header = ({ isSidebarOpen, setIsSidebarOpen }) => {
             </>
           )}
         </div>
-        <div className="flex bg-white items-center px-4 py-1 rounded-full">
-          <SearchIcon className="text-gray-500" />
-          <input
-            type="text"
-            placeholder="Search Your Notes"
-            className={`bg-transparent outline-none p-2 ${
-              isMobile ? "px-1" : isTablet ? "px-10" : "px-56"
-            } rounded-full`}
-          />
+        <div className="relative"> {/* Make the search container relative */}
+          <div className="flex bg-white items-center px-4 py-1 rounded-full shadow">
+            <SearchIcon className="text-gray-500" />
+            <input
+              type="text"
+              placeholder="Search Your Notes"
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+              className={`bg-transparent outline-none p-2 ${
+                isMobile ? "px-0" : isTablet ? "px-10" : "px-56"
+              } rounded-full`}
+            />
+            {searchValue && (
+              <ClearIcon
+                className="text-gray-500 cursor-pointer"
+                onClick={() => setSearchValue('')}
+              />
+            )}
+          </div>
+          {isFocused && searchValue && searchResults.length > 0 && (
+            <div className="absolute z-50 mt-1 w-full bg-white shadow-lg rounded-md">
+              {searchResults.map((result) => (
+                <div key={result.id} className="p-3 hover:bg-blue-100 cursor-pointer rounded-md flex items-center" onClick={() => navigate(`/${result.type === 'file' ? 'documents' : 'folders'}/${result.id}`)}>
+                  {result.type === 'file' ? getFileIcon(result.name) : <FolderIcon className="text-blue-400" />}
+                  <span className="ml-2">{result.name}</span>
+                </div>
+              ))}
+            </div>
+          )}
+          {isFocused && searchValue && searchResults.length === 0 && (
+            <div className="absolute z-50 mt-1 w-full bg-white cursor-pointer shadow-lg rounded-md p-3">
+              <div className="text-gray-700">Not found</div>
+            </div>
+          )}
         </div>
         <div className="flex items-center">
           {(isDesktopOrLaptop || !isTablet) && (
@@ -108,8 +253,7 @@ const Header = ({ isSidebarOpen, setIsSidebarOpen }) => {
             </div>
           )}
           <div className="relative">
-            {" "}
-            {/* Wrap the icon and dropdown in a relative div */}
+            {" "} {/* Wrap the icon and dropdown in a relative div */}
             {username ? (
               <div
                 className="bg-blue-300 hover:bg-blue-400 text-white font-semibold rounded-full w-12 h-12 flex items-center justify-center mx-2 cursor-pointer"
@@ -167,4 +311,4 @@ const Header = ({ isSidebarOpen, setIsSidebarOpen }) => {
   );
 };
 
-export default Header;
+export default Header
